235c050258b4910b6e3cf55c233c29698eee26ec
thrishank <thrishankkalluru@gmail.com>
2024-05-09 11:42:13 +0530

    fix logo:

--- src/componets/navbar.tsx
+++ src/componets/navbar.tsx
@@ -1,7 +1,7 @@
 export function Navbar() {
   return (
     <div className="shadow-md h-16 items-center flex justify-start">
-      <img className="p-2 m-2" src="public\logo.jpeg" width="80px" height="20px"></img>
+      <img className="p-2 m-2" src="/logo.jpeg" width="80px" height="20px"></img>
     </div>
   );
 }
