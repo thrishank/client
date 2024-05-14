835b340e324f866e96a9edd49e7dbda646705fc0
kmanish1 <[kmanishreddy11@gmail.com]>
2024-05-15 01:35:08 +0530

    made all changes except svg responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { LeftText } from "./pages/left";
 import { ScoreBadge } from "./UI/score_badge";
 import { useState } from "react";
 import { useKurbilScore } from "./components/kurbilScore";
+import MoonLoader from "react-spinners/MoonLoader";
+
 
 function App() {
   const [scoreBadgeVisible, setScoreBadgeVisible] = useState(false);
   const { kurbilScore, setKurbilScore } = useKurbilScore();
+  const [isLoading, setIsLoading] = useState(false);
 
   const toggleScoreBadge = () => {
     setScoreBadgeVisible(!scoreBadgeVisible);
@@ -19,14 +22,19 @@ function App() {
     <>
       <div className={scoreBadgeVisible ? "bg-gray-300" : ""}>
         <Navbar />
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <LeftText
-            toggleScoreBadge={toggleScoreBadge}
-            setKurbilScore={setKurbilScore}
-            kurbilScore={kurbilScore}
-          />
-          <Right />
+        <div className={isLoading?"hidden":"md:grid md:grid-cols-2 flex flex-col-reverse"}>
+            <LeftText
+              toggleScoreBadge={toggleScoreBadge}
+              setKurbilScore={setKurbilScore}
+              kurbilScore={kurbilScore}
+              setIsLoading={setIsLoading}
+              isLoading={isLoading}
+            />
+            <Right />
+
         </div>
+        <div className={isLoading?"flex justify-center items-center mt-4":"hidden"}><MoonLoader  size={200} color="#d9a831"></MoonLoader></div>
+
         {scoreBadgeVisible && (
           <div className="flex items-center justify-center absolute top-0 w-full h-full z-50">
             <ScoreBadge handleClose={hideScoreBadge} score={kurbilScore} />
